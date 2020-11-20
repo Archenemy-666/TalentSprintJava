71dd7c2307eae3e7b5e452399aa5610bef2d51f1
Arch-enemy <csiddu98@gmail.com>
2020-11-20 22:40:39 +0530

    completed a really good hospital admin framework type assignment .

---
+++ Workspace/Assignment/Hospital/src/HospitalService.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class HospitalService {
+    private ArrayList<Hospital> hospitalArray = new ArrayList<Hospital>();
+    private TreeMap<Integer, String> hospitalMap = new TreeMap<Integer, String>();
+
+    public int addHospital(Hospital c) {
+
+        hospitalArray.add(c);
+        return c.getHospitalCode();
+    }
+
+    public TreeMap<Integer, String> getHospitals() {
+
+        for (Hospital h : hospitalArray) {
+            hospitalMap.put(h.getHospitalCode(), h.getHospitalName());
+        }
+        return hospitalMap;
+
+    }
+
+    public Hospital getHospitalDetails(int hospitalCode) {
+        Hospital temp = null;
+        for (Hospital h : hospitalArray) {
+            if (h.getHospitalCode() == hospitalCode) {
+                temp =  h;
+            } else {
+                temp = null ;
+            }
+
+        }
+        return temp ;
+    }
+}
