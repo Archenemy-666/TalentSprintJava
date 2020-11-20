78a068bfad1d01ea25cb85295bdd2bcc885325eb
Arch-enemy <csiddu98@gmail.com>
2020-11-20 09:29:47 +0530

    made changes by including a sorting method in order to find similar terms

--- Workspace/Assignment/UsingIterations/src/BookDetailsFor.java
+++ Workspace/Assignment/UsingIterations/src/BookDetailsFor.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class BookDetailsFor {
     public static void main(String[] args){
@@ -31,6 +29,34 @@ public class BookDetailsFor {
             if(count >= 2 ){
                 System.out.println(bookPublisher);
             }
+        }
+
+
+
+        //Trying to use sorting for checking the common elements in the list
+
+        Collections.sort(books,new PublisherSort());
+
+        Iterator<Book> value = books.iterator();
+        Book temp = new Book();
+        temp.setPublisher("unknown");
+        int count = 0;
+        System.out.println("inside temp is : "+temp);
+
+        System.out.println("no temp is : "+temp);
+        while(value.hasNext()){
+            Book b = value.next();
+            if(temp.getPublisher().equals("unknown")){
+                temp = b ;
+            }
+            if(b.getPublisher().equals(temp.getPublisher())){
+                count ++ ;
+                System.out.println("are similar :  "+temp.getName()+" ,  "+ b.getName());
+
+            }
+
+
+
         }
 
     }
